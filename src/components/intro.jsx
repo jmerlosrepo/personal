c07c6f241e06624ac99f145119bdb7a1b3861dad
jmerlosrepo <77901200+jmerlosrepo@users.noreply.github.com>
2021-03-03 18:04:58 -0600

    Update intro.jsx

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,31 +1,19 @@
 import React from "react";
+import ReactHtmlParser from 'react-html-parser'
+import * as personalConstants from '../constants';
 
-const Intro = () => {
+const Intro = ({introName, career, presentation, profilePic}) => {
   return (
     <div className="container container-margins row">
       <div className="col-md-6">
-        <div className="picture"></div>
+        <div className="picture" style={{backgroundImage: `url("${personalConstants.HOST}:${personalConstants.PORT}/assets/${profilePic}")`}}></div>
       </div>
       <div className="col-md-6">
         <div className="introTitle">
-          <h2>Hi, I'm Jesus Merlos</h2>
-          <h3>SOFTWARE DEVELOPER</h3>
+          <h2>Hi, I'm {introName}</h2>
+          <h3>{career.toUpperCase()}</h3>
         </div>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo
-          asperiores adipisci dolore odit sed ducimus tempora laborum a nam
-          incidunt quisquam laboriosam, itaque eos reprehenderit odio aut
-          deserunt, voluptatem soluta ratione impedit earum, voluptates et unde?
-          Reprehenderit reiciendis, asperiores soluta repudiandae, libero vel
-          sunt fugiat consectetur odit placeat sed quas. Earum perferendis
-          veritatis vitae iusto quod? Earum iste eligendi nulla!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-          laboriosam quae, tenetur laborum tempora nihil! Laudantium quod quia
-          repellendus possimus, sunt officiis alias corrupti quasi soluta
-          repudiandae in. Delectus, a.
-        </p>
+        {ReactHtmlParser(presentation)}
       </div>
     </div>
   );
