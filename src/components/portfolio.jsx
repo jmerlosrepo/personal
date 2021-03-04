4fbbae8c87a022184080caccb981d8616653a196
jmerlosrepo <77901200+jmerlosrepo@users.noreply.github.com>
2021-03-03 20:18:38 -0600

    Update portfolio.jsx

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import PortfolioListElement from './portfolioListElement';
 
-const Portfolio = () => {
-  return <div>Portfolio</div>;
+const Portfolio = ({portfolios}) => {
+  return (
+    <div className="container container-margins">
+      <table className="table table-striped">
+        <thead className="thead-light">
+          <tr>
+            <th scope="col">Project</th>
+            <th scope="col">Description</th>
+            <th scope="col">URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {portfolios.map(portfolio => <PortfolioListElement key={portfolio.id} porfolioElement={portfolio} />)}
+        </tbody>
+      </table>      
+    </div>
+  );
 };
 
 export default Portfolio;
