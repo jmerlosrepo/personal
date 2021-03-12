93ca49c2c9f213fd25b6096ea07bd198b7bc2d49
jmerlosrepo <77901200+jmerlosrepo@users.noreply.github.com>
2021-03-12 15:20:26 -0600

    Update intro.jsx

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import ReactHtmlParser from 'react-html-parser'
+import ReactHtmlParser from 'react-html-parser';
 import * as personalConstants from '../constants';
 
 const Intro = ({introName, career, presentation, profilePic}) => {
   return (
-    <div className="container container-margins row">
-      <div className="col-md-6">
+    <div className="container container-margins intro">
+      <div className="introPic">
         <div className="picture" style={{backgroundImage: `url("${personalConstants.HOST}:${personalConstants.PORT}/assets/${profilePic}")`}}></div>
       </div>
-      <div className="col-md-6">
+      <div className="IntroBio">
         <div className="introTitle">
           <h2>Hi, I'm {introName}</h2>
           <h3>{career.toUpperCase()}</h3>
