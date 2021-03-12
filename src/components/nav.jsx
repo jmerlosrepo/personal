ea290e940088167403ef998d046d2a72c7101877
jmerlosrepo <77901200+jmerlosrepo@users.noreply.github.com>
2021-03-12 15:20:46 -0600

    Update nav.jsx

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
+
+  const clickHamburger = () =>{
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar navbar-expand-lg navbar-dark bg-dark") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar navbar-expand-lg navbar-dark bg-dark";
+    }
+  }
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark" id="myTopnav">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <Link className="nav-link" to="/">
@@ -25,6 +35,7 @@ const Nav = () => {
             Contact
           </Link>
         </li>
+        <li onClick={clickHamburger} className="icon"><i className="fa fa-bars"></i></li>
       </ul>
     </nav>
   );
