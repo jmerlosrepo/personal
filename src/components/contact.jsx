6bc1e4ebe9936215e73ca31320fe84ec6b1f2a54
jmerlosrepo <77901200+jmerlosrepo@users.noreply.github.com>
2021-03-12 16:34:39 -0600

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,28 +1,44 @@
 import React from "react";
 import ReactHtmlParser from 'react-html-parser';
 
-const Contact = ({info}) => {
+const Contact = ({info, onSend}) => {
+
+  const contactMessage = {
+    email: "",
+    messge: ""
+  }
+
+  const handleInputChange = (e) => {
+    contactMessage[e.target.name] = e.target.value;
+  }
+
+  const hadleSubmit = (e) => {
+    e.preventDefault();
+    onSend(e, contactMessage);
+    document.getElementById('contactForm').reset();
+  }
+
   return (
     <div className="container container-margins">
       <p>{ReactHtmlParser(info.disclaimer)}</p>
-      <form>
+      <form id="contactForm" onSubmit={hadleSubmit}>
         <h3>Contact Me</h3>
         <div className="formContainer">
-          <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input className="form-control" type="email" name="email"/>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email</label>
+            <input onChange={handleInputChange} className="form-control" type="email" name="email"/>
           </div>
           <div className="mb-3">
-            <label for="message" class="form-label">Message</label>
-            <textarea className="form-control" name="message" cols="30" rows="10"></textarea>
+            <label htmlFor="message" className="form-label">Message</label>
+            <textarea onChange={handleInputChange} className="form-control" name="message" cols="30" rows="10"></textarea>
           </div>
           <div className="mb-3 text-right">
             <button className="btn btn-secondary right-align">Send</button>
           </div>
         </div>
       </form>
-      <p>My email address is: <strong>{info.email}</strong></p>
-      <p><strong>Thanks for reaching out to me!</strong></p>
+      <div>My email address is: <strong>{info.email}</strong></div>
+      <div><strong>Thanks for reaching out to me!</strong></div>
     </div>
   );
 };
